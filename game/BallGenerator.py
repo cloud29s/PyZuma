a453334bdfc9a6c9fe17e9ccf0a8b949774150f3
cloud29s <74770496+cloud29s@users.noreply.github.com>
2025-03-01 15:05:04 +0700

    Add files via upload

---
+++ game/BallGenerator.py
@@ -0,0 +1,94 @@
+from game.Settings import *
+from game.Sprites import Ball
+import random
+
+
+class BallGenerator:
+    def __init__(self, path, number, score_manager):
+        self.score_manager = score_manager
+        self.path = path
+        self.colors = [BLUE, RED, GREEN, YELLOW]
+        self.balls = []
+        self.number_to_generate = number
+        self.number_of_generated = 0
+
+        self.reverse = False
+        self.pause = False
+
+    def generate(self):
+        if self.number_of_generated < self.number_to_generate:
+            if len(self.balls) == 0 or \
+                    self.balls[0].pos_in_path >= 2 * BALL_RADIUS // \
+                    self.path.step:
+                self.balls.insert(0, Ball(random.choice(self.colors), 0,
+                                          self.path))
+                self.number_of_generated += 1
+
+    def move_stopped_ball(self, i):
+        if not self.balls[i].can_move:
+            if i == 0:
+                self.balls[i].can_move = True
+
+            elif self.balls[i - 1].can_move and \
+                    self.balls[i - 1].rect.colliderect(self.balls[i].rect):
+                self.balls[i].can_move = True
+
+    def update_balls(self):
+        for i in range(len(self.balls)):
+            self.balls[i].update()
+            self.move_stopped_ball(i)
+
+    def update_chain(self):
+        for i in range(1, len(self.balls)):
+            left_ball = self.balls[i - 1]
+            right_ball = self.balls[i]
+            if right_ball.pos_in_path - left_ball.pos_in_path > 20:
+                if left_ball.color == right_ball.color:
+                    self.join_balls(i - 1)
+                else:
+                    self.stop_balls(i)
+
+    def update(self):
+        self.update_chain()
+        if not self.reverse and not self.pause:
+            self.update_balls()
+        if len(self.balls) == 0 and self.number_of_generated == \
+                self.number_to_generate:
+            self.score_manager.win()
+
+    def draw(self, screen):
+        for ball in self.balls:
+            ball.draw(screen)
+
+    def get_available_colors(self):
+        return [ball.color for ball in self.balls]
+
+    def insert(self, index, shooting_ball):
+        ball = self.convert_shooting_ball(index, shooting_ball)
+        self.balls.insert(index + 1, ball)
+        for i in range(index + 2, len(self.balls)):
+            if self.balls[i].pos_in_path - self.balls[i - 1].pos_in_path >= \
+                    2 * BALL_RADIUS // self.path.step:
+                break
+            self.balls[i].set_position(self.count_next_pos(i - 1))
+
+    def convert_shooting_ball(self, index, shooting_ball):
+        ball = Ball(shooting_ball.color,
+                    self.count_next_pos(index), self.path)
+        ball.can_move = self.balls[index].can_move
+        return ball
+
+    def destroy(self, chain):
+        for ball in chain:
+            self.balls.remove(ball)
+
+    def join_balls(self, index):
+        for i in range(index, len(self.balls)):
+            self.balls[i].set_position(self.count_next_pos(i - 1))
+
+    def stop_balls(self, tail_index):
+        for i in range(tail_index, len(self.balls)):
+            self.balls[i].can_move = False
+
+    def count_next_pos(self, index):
+        return self.balls[index].pos_in_path + 2 * BALL_RADIUS // self.path.step
---
+++ game/Sprites.py
@@ -0,0 +1,136 @@
+import math
+from game.Settings import *
+from game.Ui import BONUS_IMAGES
+import game.images
+
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, color, pos_in_path, path):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.color = color
+
+        self.path = path
+        self.pos_in_path = pos_in_path
+
+        self.image = pygame.Surface(BALL_SIZE)
+        self.pos = self.path.positions[self.pos_in_path]
+        self.rect = self.image.get_rect(center=(round(self.pos.x),
+                                                round(self.pos.y)))
+
+        self.can_move = True
+        self.bonus = None
+
+    def set_bonus(self, bonus):
+        self.bonus = bonus
+
+    def set_position(self, pos_in_path):
+        self.pos_in_path = pos_in_path
+        self.pos = self.path.positions[self.pos_in_path]
+        self.rect.center = (round(self.pos.x), round(self.pos.y))
+
+    def update(self):
+        if self.can_move:
+            self.move(1)
+
+    def move(self, steps):
+        self.pos_in_path += steps
+        if self.pos_in_path >= 0:
+            self.pos = pygame.math.Vector2(
+                self.path.positions[self.pos_in_path])
+            self.rect.center = (round(self.pos.x), round(self.pos.y))
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, self.rect.center, BALL_RADIUS)
+        if self.bonus is not None:
+            screen.blit(pygame.image.load(
+                BONUS_IMAGES[self.bonus][self.color]),
+                (self.rect.x, self.rect.y))
+
+    def __eq__(self, other):
+        return self.color == other.color and \
+               self.rect.center == other.rect.center and \
+               self.can_move == other.can_move
+
+
+class ShootingBall(pygame.sprite.Sprite):
+    def __init__(self, color, pos=SCREEN_CENTER):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.color = color
+
+        self.image = pygame.Surface(BALL_SIZE)
+        self.rect = self.image.get_rect(center=pos)
+
+        self.target = (0, 0)
+        self.speed = 15
+
+        self.time = None
+
+    def set_time(self, time):
+        self.time = time
+
+    def set_target(self, target):
+        self.target = (target[0] - self.rect.center[0],
+                       target[1] - self.rect.center[1])
+        length = math.hypot(*self.target)
+        self.target = (self.target[0] / length, self.target[1] / length)
+
+    def update(self):
+        self.rect.center = (self.rect.center[0] + self.target[0] * self.speed,
+                            self.rect.center[1] + self.target[1] * self.speed)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, self.rect.center, BALL_RADIUS)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, level):
+        pygame.sprite.Sprite.__init__(self)
+
+        if level == 1:
+            self.pos = (530, 330)
+        else:
+            self.pos = SCREEN_CENTER
+
+        self.original_image = pygame.transform.smoothscale(
+            pygame.image.load('game/images/player.png'), PLAYER_SIZE)
+        self.original_image.set_colorkey(BLACK)
+
+        self.image = self.original_image
+
+        self.rect = self.image.get_rect(center=self.pos)
+
+        self.angle = 0
+
+    def update(self):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        rel_x, rel_y = mouse_x - self.rect.x, mouse_y - self.rect.y
+        self.angle = (180 / math.pi) * (-math.atan2(rel_y, rel_x)) + 90
+        self.image = pygame.transform.rotate(self.original_image, self.angle)
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect(center=self.rect.center)
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Finish(pygame.sprite.Sprite):
+    def __init__(self, path, balls, score_manager):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.balls = balls
+        self.score_manager = score_manager
+
+        self.image = pygame.transform.smoothscale(
+            pygame.image.load("game/images/star.png"), (80, 80))
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect(center=path.positions[-1])
+
+    def update(self):
+        for ball in self.balls:
+            if self.rect.colliderect(ball.rect):
+                self.score_manager.lose()
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
